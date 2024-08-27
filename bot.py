e1a36b1b0a29c1ac2a6e315b1c1a30bf2f197dd1
AlexeyGold2077 <alexgold2077@yandex.ru>
2024-08-27 15:11:46 +0300

    add SendMessageDTO" "

--- bot.py
+++ bot.py
@@ -2,32 +2,45 @@ import telebot
 import mindgate_pywrapper
 import _private
 
-bot = telebot.TeleBot(_private.TOKEN)
+bot = telebot.TeleBot(_private.TOKEN_DEV)
 
-system_message = "Be brief"
+SYSTEM_MESSAGE = "Be brief"
 
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, """🤙 Привет! Я ваш AI помощник. Чем могу помочь?""")
-    mindgate_pywrapper.sendMessageAsSystem(message.chat.id, system_message)
+def command_start(message):
+    mindgate_pywrapper.sendMessageAsSystem(message.chat.id, SYSTEM_MESSAGE)
+    bot.reply_to(message, '🤙 Привет! Я ваш AI помощник. Чем могу помочь?')
 
 
 @bot.message_handler(commands=['reset'])
-def clearMessages(message):
+def command_reset(message):
     mindgate_pywrapper.clearMessages(message.chat.id)
-    mindgate_pywrapper.sendMessageAsSystem(message.chat.id, system_message)
-    bot.send_message(message.chat.id, "MEMORY_RESET_SUCCESSFUL")
+    mindgate_pywrapper.sendMessageAsSystem(message.chat.id, SYSTEM_MESSAGE)
+    bot.send_message(message.chat.id, "🫨 Память сброшена!")
+    bot.reply_to(message, '🤙 Привет! Я ваш AI помощник. Чем могу помочь?')
+
+
+@bot.message_handler(commands=['model'])
+def command_model(message):
+    mindgate_pywrapper.clearMessages(message.chat.id)
+    bot.send_message(message.chat.id, "Вы не должны быть здесь...")
 
 
 @bot.message_handler(content_types=['text'])
-def respond_to_message(message):
+def respond_to_text(message):
     user_message = message.text
-    response = mindgate_pywrapper.sendMessageAsUser(message.chat.id, f"""{user_message}""")
-    bot.send_message(message.chat.id, response, parse_mode="Markdown")
+    response = mindgate_pywrapper.sendMessageAsUser(message.chat.id, f'{user_message}')
+    bot.send_message(message.chat.id,
+                     response["message"] + f'\n\n⛽️ {response["total_tokens"]}',
+                     parse_mode="Markdown")
+
+    print(response["total_tokens"], response["prompt_tokens"], response["completion_tokens"])
+
     print("\n\nid - " + str(message.chat.id))
+    print("username - " + str(message.from_user.username))
     print("user - " + user_message)
-    print("bot - " + response)
+    print("bot - " + response["message"])
 
 
 bot.infinity_polling()
--- mindgate_pywrapper.py
+++ mindgate_pywrapper.py
@@ -1,6 +1,6 @@
 import requests
 
-api_base_url = "http://172.17.0.1:8080"
+api_base_url = "http://localhost:8080"
 
 
 def sendMessageAsUser(user_id, message):
@@ -10,7 +10,8 @@ def sendMessageAsUser(user_id, message):
 
     response = requests.post(url=url, headers=headers, params=params)
     if response.status_code == 200:
-        return response.text
+        response_json = response.json()
+        return response_json
     else:
         print(f"Error: {response.status_code}")
         return "ERROR"
@@ -23,7 +24,8 @@ def sendMessageAsSystem(user_id, message):
 
     response = requests.post(url=url, headers=headers, params=params)
     if response.status_code == 200:
-        return response.text
+        response_json = response.json()
+        return response_json
     else:
         print(f"Error: {response.status_code}")
         return "ERROR"
@@ -36,7 +38,36 @@ def clearMessages(user_id):
 
     response = requests.patch(url=url, headers=headers, params=params)
     if response.status_code == 200:
-        return response.text
+        response_json = response.json()
+        return response_json
+    else:
+        print(f"Error: {response.status_code}")
+        return "ERROR"
+
+
+def getModel(user_id, message):
+    headers = {'Content-Type': 'application/json'}
+    url = api_base_url + "/getModel"
+    params = {"id": user_id}
+
+    response = requests.get(url=url, headers=headers, params=params)
+    if response.status_code == 200:
+        response_json = response.json()
+        return response_json
+    else:
+        print(f"Error: {response.status_code}")
+        return "ERROR"
+
+
+def setModel(user_id, model):
+    headers = {'Content-Type': 'application/json'}
+    url = api_base_url + "/setModel"
+    params = {"id": user_id, "model": model}
+
+    response = requests.patch(url=url, headers=headers, params=params)
+    if response.status_code == 200:
+        response_json = response.json()
+        return response_json
     else:
         print(f"Error: {response.status_code}")
         return "ERROR"
