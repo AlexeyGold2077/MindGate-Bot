dc5e31dc934e57a2c25755fa9a7862ae41160e77
AlexeyGold2077 <alexgold2077@yandex.ru>
2024-08-29 17:06:18 +0300

    before adding new structure

--- mindgate_pywrapper.py
+++ mindgate_pywrapper.py
@@ -3,6 +3,20 @@ import requests
 api_base_url = "http://localhost:8080"
 
 
+def sender(mapping_url, params):
+    headers = {'Content-Type': 'application/json'}
+    url = api_base_url + mapping_url
+
+    response = requests.post(url=url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        response_json = response.json()
+        return response_json
+    else:
+        print(f"Error: {response.status_code}")
+        return "ERROR"
+
+
 def sendMessageAsUser(user_id, message):
     headers = {'Content-Type': 'application/json'}
     url = api_base_url + "/sendMessage"
